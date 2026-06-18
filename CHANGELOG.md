39197b276b10f4f5a414cd0d1e47b09b83f10211
Pablo Rica <pabloricaaldeguer@gmail.com>
2026-06-18 20:29:20 +0200

    Header | Small logo
    ## [0.3.4] - 2026-06-18
    
    ### Updated
    
    - [Header | Small logo](https://app.asana.com/1/1198727466849836/project/1215042979527993/task/1215794859420147)
    
    - [Top menu font to Bold instead of Light](https://app.asana.com/1/1198727466849836/project/1215042979527993/task/1215794859420145)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,16 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ---
 
+## [0.3.4] - 2026-06-18
+
+### Updated
+
+- [Header | Small logo](https://app.asana.com/1/1198727466849836/project/1215042979527993/task/1215794859420147)
+
+- [Top menu font to Bold instead of Light](https://app.asana.com/1/1198727466849836/project/1215042979527993/task/1215794859420145)
+
+
+
 ## [0.3.3] - 2026-06-18
 
 ### Updated
--- wordpress/wp-content/themes/codigo/resources/views/sections/header.blade.php
+++ wordpress/wp-content/themes/codigo/resources/views/sections/header.blade.php
@@ -76,7 +76,7 @@
       left-2.5 right-2.5 md:left-5 md:right-5
       grid grid-cols-6 lg:grid-cols-12"
   >
-    <a class="brand block col-span-6 md:col-span-1"
+    <a class="brand block col-span-6 md:col-span-2 md:-mr-4!"
       href="{!! home_url('/') !!}">
         @include(
           'icons.logo',
@@ -96,7 +96,7 @@
         {!! wp_nav_menu([
             'theme_location' => 'primary_navigation',
             'container' => false,
-            'menu_class' => 'text-white w-full
+            'menu_class' => 'text-white w-full font-medium
               flex gap-2 md:gap-4 lg:gap-6 items-center justify-between',
             'echo' => false
           ]
